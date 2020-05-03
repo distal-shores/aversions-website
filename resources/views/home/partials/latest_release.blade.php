e09be6978c1b93c715003d202db4bbc7c8ddb68c
distal-shores <samcoll@gmail.com>
2020-05-03 15:59:37 -0700

    Get rid of the height value on the main container div on homepage.

--- resources/views/home/partials/latest_release.blade.php
+++ resources/views/home/partials/latest_release.blade.php
@@ -1,4 +1,4 @@
-<section class="latest-release" style="height:3000px;">
+<section class="latest-release">
 	<h2>Latest Release</h2>
 	<iframe style="border: 0; width: 100%; height: 340px;" src="https://bandcamp.com/EmbeddedPlayer/album=4129799542/size=large/bgcol=333333/linkcol=ffffff/artwork=small/transparent=true/" seamless><a href="http://aversionsband.bandcamp.com/album/base-portrait-ep">Base Portrait EP by Aversions</a></iframe>
 </section>
