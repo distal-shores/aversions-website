2b3b210b819c6741096b81027253325a3b9214bb
distal-shores <samcoll@gmail.com>
2020-05-09 15:41:41 -0700

    Move images out of public storage and into public file.

--- resources/views/home/partials/footer.blade.php
+++ resources/views/home/partials/footer.blade.php
@@ -1,11 +1,11 @@
 <footer class="col-12">
 	<div class="row no-gutters footer-content">
 		<div class="col social-media">
-			<a class="social-icon circle spotify" href="https://open.spotify.com/artist/65fB37pXWPMPJrNg1ZJOpR" alt="Listen to Aversions on Spotify"><img src="{{ asset('img/footer_social_spotify.png') }}"/></a>
-			<a class="social-icon bandcamp" href="https://aversionsband.bandcamp.com" alt="Listen to or buy merch from Aversions on Bandcamp"><img src="{{ asset('img/footer_social_bandcamp.png') }}"/></a>
-			<a class="social-icon soundcloud" href="https://soundcloud.com/aversionstheband/" alt="Listen to Aversions on Soundcloud"><img src="{{ asset('img/footer_social_soundcloud.png') }}"/></a>
-			<a class="social-icon circle facebook" href="https://www.facebook.com/aversionsisaband/" alt="Aversions Facebook page"><img src="{{ asset('img/footer_social_facebook.png') }}"/></a>
-			<a class="social-icon instagram" href="https://www.instagram.com/aversionsisaband/" alt="Aversions Instagram page"><img src="{{ asset('img/footer_social_instagram.png') }}"/></a>
+			<a class="social-icon circle spotify" href="https://open.spotify.com/artist/65fB37pXWPMPJrNg1ZJOpR" alt="Listen to Aversions on Spotify"><img src="{{ asset('images/footer_social_spotify.png') }}"/></a>
+			<a class="social-icon bandcamp" href="https://aversionsband.bandcamp.com" alt="Listen to or buy merch from Aversions on Bandcamp"><img src="{{ asset('images/footer_social_bandcamp.png') }}"/></a>
+			<a class="social-icon soundcloud" href="https://soundcloud.com/aversionstheband/" alt="Listen to Aversions on Soundcloud"><img src="{{ asset('images/footer_social_soundcloud.png') }}"/></a>
+			<a class="social-icon circle facebook" href="https://www.facebook.com/aversionsisaband/" alt="Aversions Facebook page"><img src="{{ asset('images/footer_social_facebook.png') }}"/></a>
+			<a class="social-icon instagram" href="https://www.instagram.com/aversionsisaband/" alt="Aversions Instagram page"><img src="{{ asset('images/footer_social_instagram.png') }}"/></a>
 		</div>
 	</div>
 </footer>
--- resources/views/home/partials/top-nav.blade.php
+++ resources/views/home/partials/top-nav.blade.php
@@ -1,7 +1,7 @@
 <span class="retractable"></span>
 <nav class="navbar">
 	<a class="navbar-brand" href="#">
-  		<img id="logo-default" class="no-top-margin" src="{{ asset('img/home_logo-300-black.png') }}" alt="Aversions masthead logo">
-  		<img id="logo-alternate" class="no-top-margin" src="{{ asset('img/home_logo-300.png') }}" alt="Aversions alternate masthead logo">
+  		<img id="logo-default" class="no-top-margin" src="{{ asset('images/home_logo-300-black.png') }}" alt="Aversions masthead logo">
+  		<img id="logo-alternate" class="no-top-margin" src="{{ asset('images/home_logo-300.png') }}" alt="Aversions alternate masthead logo">
 	</a>
 </nav>
