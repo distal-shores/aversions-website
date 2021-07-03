5a28b016c808037f9306598ff489adaabbb33d15
distal-shores <samcoll@gmail.com>
2021-07-03 13:09:44 -0700

    Add Facebook pixel.

--- resources/views/home/partials/splash_menus.blade.php
+++ resources/views/home/partials/splash_menus.blade.php
@@ -1,10 +1,10 @@
 <div class="artwork">
     <ul class="cta-left">
         <li>
-            <a href="https://youtu.be/T-Ue2xa2xnU">Watch the video</a>
+            <a href="https://youtu.be/T-Ue2xa2xnU" onclick="fbq('trackCustom', 'Video Watch');">Watch the video</a>
         </li>
         <li>
-            <a href="https://open.spotify.com/track/3PeB7CQs7texiNiQ1umEKr">Listen on Spotify</a>
+            <a href="https://open.spotify.com/track/3PeB7CQs7texiNiQ1umEKr" onclick="fbq('trackCustom', 'Spotify Listen');">Listen on Spotify</a>
         </li>
         <li>
             <a href="https://aversionsband.bandcamp.com/track/famous-last-lines">Buy Single</a>
@@ -15,7 +15,7 @@
             <a href="https://aversionsband.bandcamp.com/album/base-portrait-ep">Buy a record</a>
         </li>
         <li style="margin-top: 33.25%;">
-            <a href="https://aversionsband.bandcamp.com/follow_me">Follow us on Bandcamp</a>
+            <a href="https://aversionsband.bandcamp.com/follow_me" onclick="fbq('trackCustom', 'Bandcamp Follow');">Follow us on Bandcamp</a>
         </li>
     </ul>
 </div>
--- resources/views/home/partials/splash_menus_mobile.blade.php
+++ resources/views/home/partials/splash_menus_mobile.blade.php
@@ -1,10 +1,10 @@
 <div class="artwork mobile">
     <ul class="ctas-mobile">
         <li>
-            <a href="https://youtu.be/T-Ue2xa2xnU">Watch the video</a>
+            <a href="https://youtu.be/T-Ue2xa2xnU" onclick="fbq('trackCustom', 'Video Watch');">Watch the video</a>
         </li>
         <li>
-            <a href="https://open.spotify.com/track/3PeB7CQs7texiNiQ1umEKr">Listen on Spotify</a>
+            <a href="https://open.spotify.com/track/3PeB7CQs7texiNiQ1umEKr" onclick="fbq('trackCustom', 'Spotify Listen');">Listen on Spotify</a>
         </li>
         <li>
             <a href="https://aversionsband.bandcamp.com">Buy Single</a>
@@ -13,7 +13,7 @@
             <a href="https://aversionsband.bandcamp.com/album/base-portrait-ep">Buy a record</a>
         </li>
         <li>
-            <a href="https://aversionsband.bandcamp.com/follow_me">Follow us on Bandcamp</a>
+            <a href="https://aversionsband.bandcamp.com/follow_me" onclick="fbq('trackCustom', 'Bandcamp Follow');">Follow us on Bandcamp</a>
         </li>
     </ul>
 </div>
