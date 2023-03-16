52aa1f8b5ec8db5a010e917553a90e6d0977a4db
distal-shores <samcoll@gmail.com>
2023-03-16 11:16:34 -0700

    Update album landing page.

--- resources/views/home/partials/footer.blade.php
+++ resources/views/home/partials/footer.blade.php
@@ -1,11 +1,13 @@
 <footer class="col-12">
 	<div class="row no-gutters footer-content">
 		<div class="col social-media">
+			<a class="social-icon tiktok" href="https://www.tiktok.com/@aversionsband" alt="Watch Aversions on Tiktok"><img src="{{ asset('images/footer_social_tiktok.png') }}"/></a>
 			<a class="social-icon circle spotify" href="https://open.spotify.com/artist/65fB37pXWPMPJrNg1ZJOpR" alt="Listen to Aversions on Spotify"><img src="{{ asset('images/footer_social_spotify.png') }}"/></a>
 			<a class="social-icon bandcamp" href="https://aversionsband.bandcamp.com" alt="Listen to or buy merch from Aversions on Bandcamp"><img src="{{ asset('images/footer_social_bandcamp.png') }}"/></a>
-			<a class="social-icon soundcloud" href="https://soundcloud.com/aversionstheband/" alt="Listen to Aversions on Soundcloud"><img src="{{ asset('images/footer_social_soundcloud.png') }}"/></a>
-			<a class="social-icon circle facebook" href="https://www.facebook.com/aversionsisaband/" alt="Aversions Facebook page"><img src="{{ asset('images/footer_social_facebook.png') }}"/></a>
-			<a class="social-icon instagram" href="https://www.instagram.com/aversionsisaband/" alt="Aversions Instagram page"><img src="{{ asset('images/footer_social_instagram.png') }}"/></a>
+			<a class="social-icon instagram" href="https://www.instagram.com/aversionsband/" alt="Aversions Instagram page"><img src="{{ asset('images/footer_social_instagram.png') }}"/></a>
+			{{-- <a class="social-icon circle facebook" href="https://www.facebook.com/aversionsisaband/" alt="Aversions Facebook page"><img src="{{ asset('images/footer_social_facebook.png') }}"/></a> --}}
+			<a class="social-icon youtube" href="https://www.youtube.com/channel/UCtugX2h4tEEV7owtjZmpGTA" alt="Aversions Youtube channel"><img src="{{ asset('images/footer_social_youtube.png') }}"/></a>
+
 		</div>
 	</div>
 </footer>
